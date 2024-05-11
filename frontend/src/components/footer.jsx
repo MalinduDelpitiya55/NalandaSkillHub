49e21470b4497d09d51ad23dc28c1bce1657cd83
gihani571 <145416548+gihani571@users.noreply.github.com>
2024-05-11 16:46:49 +0530

    number1

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -119,13 +119,13 @@ const Footer = () => {
               </p>
               <p>
                 <i className="fas fa-envelope me-3"></i>
-                info@example.com
+                info@example.cbnnom
               </p>
               <p>
                 <i className="fas fa-phone me-3"></i> + 01 255454445425
               </p>
               <p>
-                <i className="fas fa-print me-3"></i> + 01 234 567 89
+                <i className="fas fa-print me-3"></i> + 0142522221
               </p>
             </div>
             {/* Grid column */}
