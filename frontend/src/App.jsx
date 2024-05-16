d3aaceb1ae1ebf4f9a6c78501c3f60e93cbd4dc6
Malindu Delpitiya <mdelpitiya66@gmail.com>
2024-05-17 01:42:31 +0530

    before delete pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import BuyersRegistration from "./pages/buyerSignup";
 import SellerRegistration from "./pages/selllerSignup";
 import SellerRegistration12 from "./pages/registrationForSeller12";
 import Login from "./pages/login";
-import Home from "./pages/home";
 import User from "./pages/userChoosePage";
 import ForgotPassword from "./pages/fogotPassword";
+import Test from "./components/test";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -15,16 +15,14 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        
         <Route path="/user" element={<User />} />
         <Route path="/buyerRegistration" element={<BuyersRegistration />} />
         <Route path="/sellerRegistration" element={<SellerRegistration />} />
-        <Route
-          path="/sellerRegistration12"
-          element={<SellerRegistration12 />}
-        />
+        <Route path="/sellerRegistration12" element={<SellerRegistration12 />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
