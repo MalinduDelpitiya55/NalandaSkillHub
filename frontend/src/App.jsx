13b8232bd5cd94842714a2f47c5ecc4019b1e7bd
THARUSHIKITHULGODA <sandeepatharukt@gmail.com>
2024-05-19 15:47:08 +0530

    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,17 @@ import BuyersRegistration from "./pages/buyerSignup";
 import SellerRegistration from "./pages/selllerSignup";
 import SellerRegistration12 from "./pages/registrationForSeller12";
 import Login from "./pages/login";
-<<<<<<< HEAD
 import Home from "./pages/home";
 import Profile from "./pages/profile";
 import User from "./pages/userChoosePage";
 import ForgotPassword from "./pages/fogotPassword";
 import Test from "./components/test";
-=======
-<<<<<<< HEAD
+
 import Home from "./pages/home";
 import Profile from "./pages/profile";
-=======
 import User from "./pages/userChoosePage";
 import ForgotPassword from "./pages/fogotPassword";
 import Test from "./components/test";
->>>>>>> d3aaceb1ae1ebf4f9a6c78501c3f60e93cbd4dc6
->>>>>>> 7150f3e9e45eadc3cadc031174f1b3e78ef64f86
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -35,18 +30,12 @@ export default function App() {
         <Route path="/sellerRegistration" element={<SellerRegistration />} />
         <Route path="/sellerRegistration12" element={<SellerRegistration12 />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/test" element={<Test />}/>
         <Route path="/profile" element={<Profile />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/test" element={<Test />} />
-=======
-<<<<<<< HEAD
-        <Route path="/profile" element={<Profile />} />
-=======
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/test" element={<Test />} />
->>>>>>> d3aaceb1ae1ebf4f9a6c78501c3f60e93cbd4dc6
->>>>>>> 7150f3e9e45eadc3cadc031174f1b3e78ef64f86
       </Routes>
     </Router>
   );
