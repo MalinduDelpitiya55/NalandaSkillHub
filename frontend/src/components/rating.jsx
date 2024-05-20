3afb7fd789941df61778d294a7d2522227c2f247
LakshaniViranga <taviranga2001@gmail.com>
2024-05-20 21:32:29 +0530

    new correct rating form

--- frontend/src/components/rating.jsx
+++ frontend/src/components/rating.jsx
@@ -10,15 +10,16 @@ const rating= () => {
     
     <div
     style={{
-      background: '#FFF',
+      background: '#E5F3FD',
       padding: '2rem',
-      maxWidth: '576px',
+      maxWidth: '500px',
       width: '90%',
       borderRadius: '0.75rem',
       boxShadow: '8px 8px 30px rgba(0, 0, 0, .05)',
       textAlign: 'center',
-      marginLeft:'150px',
-      marginTop:'150px'
+      marginLeft:'400px',
+      marginTop:'100px',
+      marginBottom:'50px'
     }}
   >
     <h5 style={{ fontSize: '1.25rem', fontWeight: 600, marginBottom: '1rem' }}>
