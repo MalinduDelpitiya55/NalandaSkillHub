f308338095267613de00accf3610e5496095202f
AkashHirumal <akashhirumal@gmail.com>
2024-05-22 18:56:56 +0530

    navbar

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,5 @@
 
-import Navbar from "./../components/navbar2";
+import Navbar from "./../components/navbar";
 import Home from "./../components/home";
 import Footer from "./../components/footer";
 
