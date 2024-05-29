0757bb86ed1d64c8efa2928e82ca025578b5b9b4
gihani571 <145416548+gihani571@users.noreply.github.com>
2024-05-29 16:02:47 +0530

    edit profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,55 @@
-// import React from "react";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-<<<<<<< HEAD
-
-import Home from "./pages/home/home";
-import Job from "./pages/jobPost"
-import OneJobPost from "./pages/oneJob";
-=======
-import BuyersRegistration from "./pages/signup/buyer/buyerSignup";
-import SellerRegistration from "./pages/signup/seller/selllerSignup";
-import SellerRegistration12 from "./pages/signup/seller/registrationForSeller12";
-import Login from "./pages/login/login";
-import Home from "./pages/home/home";
-import Job from "./pages/jobPost";
+import BuyersRegistration from "./components/ragistration/registrationForBuyers";
+import SellerRegistration from "./components/ragistration/registrationForSeller";
+import SellerRegistration12 from "./components/ragistration/registrationForSeller12";
+import Login from "./components/login/login";
+import Home from "./components/home/home";
+import Job from "./components/gigs/job";
 import OneJobPost from "./pages/gig details/oneJob";
-import Profile from "./pages/profile/profile";
-import ProfileEdit from "./pages/profile/profileEdit";
+import Profile from "./components/profile/profile";
+import ProfileEdit from "./components/profile/profileedit";
 import Usertable from "./components/tables/usertable";
 import User from "./pages/login/userChoosePage";
 import ForgotPassword from "./pages/forgotPassword/fogotPassword";
 import Dashboard from "./pages/dashboards/dashboard";
+// import Test from "./components/test";
+import AdminRequirement from "./pages/adminJobRequ";
 import Rating from "./pages/ratingForm";
 import Review from "./pages/reviewDisplay";
 import ActiveOrders from "./pages/order/activeOrder";
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/buyerRegistration" element={<BuyersRegistration />} />
+        <Route path="/sellerRegistration" element={<SellerRegistration />} />
+        <Route
+          path="/sellerRegistration12"
+          element={<SellerRegistration12 />}
+        />
+        <Route path="/login" element={<Login />} />
         <Route path="/jobpost" element={<Job />} />
         <Route path="/onejobpost" element={<OneJobPost />} />
-<<<<<<< HEAD
-
-=======
         <Route path="/profile" element={<Profile />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/test" element={<Test />} /> */}
+        <Route path="/adminJobRequ" element={<AdminRequirement />} />
         <Route path="/Profileedit" element={<ProfileEdit />} />
         <Route path="/adminusertable" element={<Usertable />} />
         <Route path="/ratingForm" element={<Rating />} />
         <Route path="/reviewDisplay" element={<Review />} />
         <Route path="/activeorders" element={<ActiveOrders />} />
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
+
       </Routes>
     </Router>
   );
--- frontend/src/components/profile/profileedit.jsx
+++ frontend/src/components/profile/profileedit.jsx
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 //import React from 'react';
-=======
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Profilepic from "../../assets/images/profile/profile-img.jpg";
 import "./../css/profile.css";
 import Tagify from "../ragistration/skillsInputForRegister";
-<<<<<<< HEAD
-
-const CVSection = () => {
-=======
 import {useState, useEffect} from "react";
 import Axios from "axios";
+
+
 
 const CVSection = () => {
   const [formData, setFormData] = useState({
@@ -97,13 +92,11 @@ const CVSection = () => {
     }
   };
 
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
   return (
     <>
       {/* CSS */}
       <style>
         {`
-<<<<<<< HEAD
 
         .editp{
           background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
@@ -131,7 +124,7 @@ const CVSection = () => {
       }
       
            
-=======
+
         .editp {
           background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
         }
@@ -147,17 +140,14 @@ const CVSection = () => {
         .year {
           width: 150px;
         }
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
         `}
       </style>
 
       <div className="editp">
         <section className="container" id="cv">
-<<<<<<< HEAD
           <div className=" row">
-=======
+
           <div className="row">
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
             <div className="col-lg-4 mt-5 order-lg-0 order-1 text-center">
               <img
                 className="img-fluid rounded-circle border border-5 border-dark-subtle"
@@ -168,19 +158,14 @@ const CVSection = () => {
                 Akash Hirumal
               </h2>
               <ul className="list-unstyled contact-info">
-<<<<<<< HEAD
                 {/* <li id='role'>Logo Specialist and Graphic Designer</li> */}
-=======
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
+
                 <input
                   type="text"
                   id="name"
                   name="name"
                   className="col-12"
-<<<<<<< HEAD
-=======
                   onChange={handleChange}
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
                   placeholder="Logo Specialist and Graphic Designer"
                 />
               </ul>
@@ -188,7 +173,6 @@ const CVSection = () => {
               <h5>Technical Skills</h5>
               <ul className="list-unstyled">
                 <Tagify />
-<<<<<<< HEAD
                 {/* <li>HTML</li>
               <li>CSS</li>
               <li>JavaScript</li> */}
@@ -223,7 +207,6 @@ const CVSection = () => {
               <li>Interest 2</li>
               <li>Interest 3</li> */}
                 {/* Add or remove interests as needed */}
-=======
               </ul>
               <h5>Personal Interests</h5>
               <ul className="list-unstyled">
@@ -239,32 +222,24 @@ const CVSection = () => {
                     required
                   />
                 ))}
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
               </ul>
             </div>
             <div className="col-lg-7 order-lg-1 order-0 mt-5 text-start">
               <h3>Profile</h3>
               <hr />
-<<<<<<< HEAD
               {/* <p>Write a brief description of yourself, your background, and your goals. Highlight your key strengths and experiences.</p> */}
-=======
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
               <textarea
                 className="descriptionp form-control"
                 id="description"
                 name="description"
                 rows="5"
-<<<<<<< HEAD
-=======
                 onChange={handleChange}
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
                 placeholder="Write a brief description of yourself, your background, and your goals. Highlight your key strengths and experiences."
               />
               <br />
               <h3>Highlights of Qualifications</h3>
               <hr />
               <ul>
-<<<<<<< HEAD
                 <input
                   type="text"
                   id="qualification1"
@@ -293,7 +268,6 @@ const CVSection = () => {
               <li>Qualification 2</li>
               <li>Qualification 3</li> */}
                 {/* Add or remove qualifications as needed */}
-=======
                 {formData.qualifications.map((qualification, index) => (
                   <input
                     key={index}
@@ -306,12 +280,10 @@ const CVSection = () => {
                     required
                   />
                 ))}
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
               </ul>
               <h3>Education</h3>
               <hr />
               <dl>
-<<<<<<< HEAD
                 <input
                   type="text"
                   id="year1"
@@ -353,7 +325,6 @@ const CVSection = () => {
                 {/* <dt>Year - Year</dt>
               <dd>University/Institution</dd>
               <dd>Degree/Program</dd> */}
-=======
                 {formData.education.map((edu, index) => (
                   <div key={index}>
                     <input
@@ -384,12 +355,10 @@ const CVSection = () => {
                     />
                   </div>
                 ))}
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
               </dl>
               <h3>Additional Courses & Training</h3>
               <hr />
               <dl>
-<<<<<<< HEAD
                 <input
                   type="text"
                   id="year2"
@@ -461,12 +430,10 @@ const CVSection = () => {
                     />
                   </div>
                 ))}
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
               </dl>
               <h3>Work Experience</h3>
               <hr />
               <dl>
-<<<<<<< HEAD
                 <input
                   type="text"
                   id="year3"
@@ -499,41 +466,10 @@ const CVSection = () => {
               <dd>Job Position</dd>
               <dd>Company/Organization</dd> */}
                 {/* Add or remove work experience entries as needed */}
-=======
-                {formData.experiences.map((experience, index) => (
-                  <div key={index}>
-                    <input
-                      type="text"
-                      id={`experiences-${index}-year`}
-                      name={`experiences-${index}-year`}
-                      className="year col-12"
-                      onChange={handleChange}
-                      placeholder="Year"
-                    />
-                    <input
-                      type="text"
-                      id={`experiences-${index}-position`}
-                      name={`experiences-${index}-position`}
-                      className="experience col-12"
-                      onChange={handleChange}
-                      placeholder="Position"
-                    />
-                    <input
-                      type="text"
-                      id={`experiences-${index}-organization`}
-                      name={`experiences-${index}-organization`}
-                      className="experience col-12"
-                      onChange={handleChange}
-                      placeholder="Organization"
-                    />
-                  </div>
-                ))}
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
               </dl>
               <h3>Extracurricular Activities</h3>
               <hr />
               <dl>
-<<<<<<< HEAD
                 <input
                   type="text"
                   id="organization"
@@ -587,7 +523,6 @@ const CVSection = () => {
               <br />
             </div>
           </div>
-=======
                 {formData.extracurriculars.map((activity, index) => (
                   <div key={index}>
                     <input
@@ -616,9 +551,8 @@ const CVSection = () => {
                     />
                   </div>
                 ))}
-              </dl>
-            </div>
-          </div>
+              </div>
+            
           <div className="text-center">
             <button
               className="btn btn-primary"
@@ -628,7 +562,6 @@ const CVSection = () => {
               Save Changes
             </button>
           </div>
->>>>>>> 342d9ae853afea29d5efa953e184211116e2b06b
         </section>
       </div>
     </>
